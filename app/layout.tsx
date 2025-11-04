c9b8708a02b9c360b6b75bbf1ed19421501734f7
Arthur Ferreira Da Silva - Assistente de TI - (41)3310-4321 <arthur.silva@hopr.com.br>
2025-11-04 12:19:20 -0300

    Ajuste Logo Inicio 1.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,12 @@ import type { Metadata } from 'next';
 import { Lato } from 'next/font/google';
 import './globals.css';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const lato = Lato({
   subsets: ['latin'],
   weight: ['400', '700', '900'],
 });
-// -----------------------------------
 
 export const metadata: Metadata = {
   title: 'HOPR STORE - Bazar',
@@ -24,15 +24,18 @@ export default function RootLayout({
       <body className={`${lato.className} bg-white text-black`}>
         <header className="w-full bg-green-700 text-white p-4 shadow-md">
           <div className="container mx-auto flex justify-between items-center">
-            <div className="flex items-center gap-4">
-              <Image
-                src="/logo-hopr.png"
-                alt="Logo Hospital de Olhos do Paraná"
-                width={150}
-                height={40}
-                priority
-              />
-            </div>
+            <Link href="/">
+              <div className="flex items-center gap-4 cursor-pointer">
+                <Image
+                  src="/logo-hopr.png"
+                  alt="Logo Hospital de Olhos do Paraná - Voltar ao Início"
+                  width={150}
+                  height={40}
+                  priority
+                />
+              </div>
+            </Link>
+
             <span className="text-lg">HOPR STORE</span>
           </div>
         </header>
